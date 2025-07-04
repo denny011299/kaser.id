c8dcf0aeb64c06004520094a0392b116ef68586f
Eric-Jonathan <ericjonathanliem@gmail.com>
2025-07-04 17:21:18 +0700

    Tambahan Payment Setting

--- resources/views/Backoffice/General_jsBo.blade.php
+++ resources/views/Backoffice/General_jsBo.blade.php
@@ -28,6 +28,24 @@
 
 
 <script>
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    
       feather.replace();
     function LoadingButton(id) {
         $(id).html(`
